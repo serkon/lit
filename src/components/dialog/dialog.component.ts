cfb6405412cc995e84992155efcd048d252d50c3
Serkan Konakcı <serkan.konakci@timusnetworks.com>
2025-03-16 22:53:25 +0300

    feat(components): integrate dialog component to project

---
+++ src/components/dialog/dialog.component.ts
@@ -0,0 +1,48 @@
+import { LitElement, css, html, unsafeCSS } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import common from './dialog.component.scss?inline';
+
+@customElement('srknc-dialog')
+export class SrkncDialog extends LitElement {
+  static styles = [
+    css`
+      ${unsafeCSS(common)}
+      :host {
+        --grow: 1;
+        --direction: column;
+        --padding: 16px;
+      }
+    `,
+    css``,
+  ];
+
+  @property({ type: String }) title = '';
+  @property({ type: Boolean }) visible = false;
+
+  private _event = new CustomEvent('close', {
+    detail: {
+      message: false,
+    },
+  });
+
+  private close() {
+    this.visible = false;
+    this.dispatchEvent(this._event);
+  }
+
+  render() {
+    return html`
+      <div class="flex flex-column grow w-100 h-100 dialog-root items-center justify-content-center ${this.visible ? 'visible' : 'hidden'}">
+        <div class="flex flex-column position-absolute content p-6 gap-4">
+          <div class="header text-primary flex justify-content-between">
+            <h3>${this.title}</h3>
+            <button class="ghost p-1 shadow-0" @click=${this.close}><img src="/close.svg" class="w-6" alt="Close" /></button>
+          </div>
+          <div class="text-sm text-gray"><slot></slot></div>
+          <slot name="dialog-footer"></slot>
+        </div>
+        <div class="position-fixed overlay w-100 h-100" @click=${this.close}></div>
+      </div>
+    `;
+  }
+}
