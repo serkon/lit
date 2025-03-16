f563b524bc67f9e7cdc8614fbde0145e22b35102
Serkan Konakcı <serkan.konakci@timusnetworks.com>
2025-03-16 22:55:58 +0300

    refactor(pages): improve employee list layout

--- src/pages/employee/[id].page.ts
+++ src/pages/employee/[id].page.ts
@@ -1,6 +1,12 @@
-import { RouterLocation } from '@vaadin/router';
-import { LitElement, html } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { Router, RouterLocation } from '@vaadin/router';
+import { LitElement, html, css, unsafeCSS } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+import { live } from 'lit/directives/live.js';
+
+import common from '@/assets/styles/_common.scss?inline';
+import { Employee, useEmplooyeeStore } from '@/stores/employee.store';
+import { pinia } from '@/utils/store-create.util';
+import { departments, positions } from '@/utils/employee-generator.util';
 
 @customElement('employee-detail')
 export class EmployeeDetail extends LitElement {
@@ -14,19 +20,123 @@ export class EmployeeDetail extends LitElement {
     // ],
   };
 
-  @property({ type: String })
-  employeeId = '';
-  location!: RouterLocation;
+  @state() private _employeeId = '';
+  @state() private _localEmployee: Employee = {} as Employee;
+  public location!: RouterLocation;
+  private employeeStore = useEmplooyeeStore(pinia);
+  private unsubscribe: (() => void) | null = null;
 
   override connectedCallback() {
     super.connectedCallback();
-    this.employeeId = this.location.params.id as string;
+    this._employeeId = this.location.params.id as string;
+    this._localEmployee = JSON.parse(JSON.stringify(this.employeeStore.get(Number(this._employeeId)))) || ({} as Employee);
+    this.unsubscribe = this.employeeStore.$subscribe(() => {
+      this._localEmployee = this.employeeStore.get(Number(this._employeeId)) || ({} as Employee);
+      this.requestUpdate();
+    });
+  }
+
+  private get employee() {
+    return this.employeeStore.get(Number(this._employeeId));
+  }
+
+  override disconnectedCallback() {
+    super.disconnectedCallback();
+    this.unsubscribe?.();
+  }
+
+  private _handleInput(e: Event) {
+    const target = e.target as HTMLInputElement;
+    console.log(target.value, target.name);
+
+    if (target.name === 'department') {
+      const newDepartment = target.value;
+      const newPosition = positions[newDepartment as keyof typeof positions][0];
+
+      this._localEmployee = {
+        ...this._localEmployee,
+        department: newDepartment,
+        position: newPosition,
+      };
+
+      this.requestUpdate();
+    } else {
+      this._localEmployee = {
+        ...this._localEmployee,
+        [target.name]: target.value,
+      };
+    }
+  }
+
+  private _handleSubmit(e: Event) {
+    e.preventDefault();
+    this.employeeStore.update(this._localEmployee);
+    Router.go('/employee');
   }
 
   render() {
-    return html`<div>
-      <h1>Employee Detail</h1>
-      <p>deneme +${this.employeeId}+</p>
-    </div>`;
+    if (!this.employee) return html`<div>Employee not found</div>`;
+
+    return html`
+      <div class="flex flex-column gap-4">
+        <h1 class="text-primary">Employee Detail</h1>
+        <form @submit=${this._handleSubmit} class="employee-form flex flex-column gap-8 " method="post">
+          <div class="form-grid">
+            <div class="form-group">
+              <label>firstName</label>
+              <input name="firstName" .value=${live(this._localEmployee.firstName || '')} @input=${this._handleInput} required />
+            </div>
+
+            <div class="form-group">
+              <label>lastName</label>
+              <input name="lastName" .value=${live(this._localEmployee.lastName || '')} @input=${this._handleInput} required />
+            </div>
+
+            <div class="form-group">
+              <label>dateOfEmployment</label>
+              <input name="dateOfEmployment" .value=${live(this._localEmployee.dateOfEmployment || '')} @input=${this._handleInput} required />
+            </div>
+
+            <div class="form-group">
+              <label>dateOfBirth</label>
+              <input name="dateOfBirth" .value=${live(this._localEmployee.dateOfBirth || '')} @input=${this._handleInput} required />
+            </div>
+
+            <div class="form-group">
+              <label>Phone:</label>
+              <input type="tel" name="phone" .value=${live(this._localEmployee.phone || '')} @input=${this._handleInput} required />
+            </div>
+
+            <div class="form-group">
+              <label>Email:</label>
+              <input type="email" name="email" .value=${live(this._localEmployee.email || '')} @input=${this._handleInput} required />
+            </div>
+
+            <!-- Department seçimi -->
+            <div class="form-group">
+              <label>department</label>
+              <select name="department" .value=${live(this._localEmployee.department)} @input=${this._handleInput} required>
+                ${departments.map((department) => html` <option .value=${department} ?selected=${this._localEmployee.department === department}>${department}</option> `)}
+              </select>
+            </div>
+
+            <!-- Position seçimi -->
+            <div class="form-group">
+              <label>position</label>
+              <select name="position" .value=${live(this._localEmployee.position)} @input=${this._handleInput} required>
+                ${positions[this._localEmployee.department as keyof typeof positions]?.map(
+                  (position) => html` <option value="${position}" ?selected=${this._localEmployee.position === position}>${position}</option> `,
+                )}
+              </select>
+            </div>
+          </div>
+          <button type="submit" class="submit-button" @click=${this._handleSubmit}>Update Employee</button>
+        </form>
+      </div>
+    `;
   }
+
+  static styles = css`
+    ${unsafeCSS(common)}
+  `;
 }
--- src/pages/employee/list.page.ts
+++ src/pages/employee/list.page.ts
@@ -1,19 +1,21 @@
 import { LitElement, html, css, unsafeCSS } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
-import styles from '@/assets/styles/_common.scss?inline';
-import { Employee, useEmplooyeeStore } from '@/stores/employee.store';
+import { customElement } from 'lit/decorators.js';
+import common from '@/assets/styles/_common.scss?inline';
+import { useEmplooyeeStore } from '@/stores/employee.store';
 import { pinia } from '@/utils/store-create.util';
 
 @customElement('employee-list')
 export class EmployeeList extends LitElement {
-  static styles = css`
-    ${unsafeCSS(styles)}
-    :host {
-      --grow: 1;
-      --direction: column;
-      --width: 100%;
-    }
-  `;
+  static styles = [
+    css`
+      ${unsafeCSS(common)}
+      :host {
+        --grow: 1;
+        --direction: column;
+      }
+    `,
+    css``,
+  ];
 
   private employeeStore = useEmplooyeeStore(pinia);
   private unsubscribe: (() => void) | null = null;
@@ -44,15 +46,16 @@ export class EmployeeList extends LitElement {
   render() {
     return html`<div class="flex flex-column">
       <h1 class="text-primary">Employee List</h1>
-      <srknc-table title="Table" counter="10" data="${JSON.stringify(this.employeeStore.employees)}"></srknc-table>
+      ${this.employeeStore.all().length}
       <button
+        class="p-2"
         @click=${() =>
-          this.employeeStore.addEmployee({
+          this.employeeStore.add({
             id: this.employeeStore.id,
             firstName: 'John',
             lastName: 'Doe',
-            dateOfEmployment: new Date('2020-01-01'),
-            dateOfBirth: new Date('1985-03-15'),
+            dateOfEmployment: '2020-01-01',
+            dateOfBirth: '1985-03-15',
             phone: '+1 123 456 7890',
             email: 'john.doe@example.com',
             department: 'Engineering',
@@ -61,6 +64,8 @@ export class EmployeeList extends LitElement {
       >
         Add Employee
       </button>
+      <srknc-table title="Table" .store="${this.employeeStore}" storeKey="employees" .idVisible="${true}">
+      </srknc-table>
     </div>`;
   }
 }
